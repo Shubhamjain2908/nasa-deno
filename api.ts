0b34187fdf2a3f26ff7e03115e2394fb8753341c
Shubham Jain <Shubhamjain2908@gmail.com>
2020-06-14 20:20:33 +0530

    populating planets data

--- api.ts
+++ api.ts
@@ -4,7 +4,15 @@ import * as planets from "./models/planets.ts";
 const router = new Router();
 
 router.get("/", (ctx) => {
-  ctx.response.body = "CKMB";
+  ctx.response.body = `
+    {___     {__      {_         {__ __        {_       
+    {_ {__   {__     {_ __     {__    {__     {_ __     
+    {__ {__  {__    {_  {__     {__          {_  {__    
+    {__  {__ {__   {__   {__      {__       {__   {__   
+    {__   {_ {__  {______ {__        {__   {______ {__  
+    {__    {_ __ {__       {__ {__    {__ {__       {__ 
+    {__      {__{__         {__  {__ __  {__         {__
+                    Mission Control API`;
 });
 
 router.get("/planets", (ctx) => {
